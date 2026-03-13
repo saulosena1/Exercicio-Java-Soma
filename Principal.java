bc9ae20252a97e1d1599225b8411bbf67ccd9938
saulosena1 <saulosfernandesc@gmail.com>
2026-03-13 03:45:07 -0300

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,23 @@
+package br.com.exercicio; // Se você digitou o nome do pacote antes, mantenha essa linha
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("--- Exercício 1: Soma de Inteiros ---");
+        
+        System.out.print("Digite o primeiro número: ");
+        int n1 = leitor.nextInt();
+        
+        System.out.print("Digite o segundo número: ");
+        int n2 = leitor.nextInt();
+        
+        int soma = n1 + n2;
+        
+        System.out.println("A soma de " + n1 + " + " + n2 + " é igual a: " + soma);
+        
+        leitor.close();
+    }
+}
